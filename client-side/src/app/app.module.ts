d09a66b183f3aafea9bb451045686d7e3cd65fa6
kaushik.dutta <kaushik.dutta@algoscale.com>
2022-11-26 01:15:39 +0530

    created initial component

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseChartComponent } from './expense-chart/expense-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ client-side/src/app/expense-chart/expense-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-chart',
+  templateUrl: './expense-chart.component.html',
+  styleUrls: ['./expense-chart.component.css']
+})
+export class ExpenseChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
