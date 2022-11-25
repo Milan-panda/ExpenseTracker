b92912162f0acf94665808258466137554125894
kaushik.dutta <kaushik.dutta@algoscale.com>
2022-11-26 03:13:15 +0530

    created initial table component with demo data

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseTableComponent } from './expense-table/expense-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
