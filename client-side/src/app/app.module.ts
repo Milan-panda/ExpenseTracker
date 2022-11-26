734f48eda9e6f7cc79b9dadaa8c9680c31f857c1
kaushik.dutta <kaushik.dutta@algoscale.com>
2022-11-26 11:33:32 +0530

    created card for displaying amount and transaction

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardDataComponent } from './card-data/card-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ client-side/src/app/card-data/card-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-data',
+  templateUrl: './card-data.component.html',
+  styleUrls: ['./card-data.component.css']
+})
+export class CardDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
