833cb2841faff236c6068e82caa6d37f62761827
kaushik.dutta <kaushik.dutta@algoscale.com>
2022-11-27 23:30:37 +0530

    added routing for login signup

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ExpenseTableComponent } from './expense-table/expense-table.component';
+import { LoginComponent } from './login/login.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',   component:DashboardComponent},
   {path:'all-expense',component:ExpenseTableComponent},
   {path:'my-profile',component:MyProfileComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
   {path:'**', component:NotFoundComponent}
 ];
 
