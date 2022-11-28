fa5a938bac1c97a665a01f5615438fca6a5827f1
Milan <milanpanda4425@gmail.com>
2022-11-28 12:47:13 +0530

    added expense api

--- server-side/routes/auth.js
+++ server-side/routes/auth.js
@@ -21,7 +21,7 @@ router.post("/", async (req, res) => {
       return res.status(401).send({ message: "Invalid Email or Password" });
 
     const token = user.generateAuthToken();
-    res.status(200).send({ data: token, message: "Logged in successfully" });
+    res.status(200).send({ data: token, userData: user, message: "Logged in successfully" });
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
   }
---
+++ server-side/routes/expenses.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Expense = require("../models/expense");
+
+router.post("/", async (req, res) => {
+  const newExpense = new Expense(req.body);
+  try {
+    const addedExpense = await newExpense.save();
+    res.status(200).json(addedExpense);
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+router.use("/", async (req, res) =>{
+  try {
+    
+    const expenses = await Expense.find({ email: req.body.email });
+    res.status(200).json(expenses);
+  } catch (error) {
+    res.status(500).send({message: "Internal Server Error"})
+  }
+})
+
+module.exports = router;
