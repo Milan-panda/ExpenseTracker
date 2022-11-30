810c7c2ff37c4017a069b8eefa49a1feb17e1350
kaushik.dutta <kaushik.dutta@algoscale.com>
2022-11-30 17:33:44 +0530

    updat form

--- client-side/src/app/my-profile/my-profile.component.ts
+++ client-side/src/app/my-profile/my-profile.component.ts
@@ -1,47 +1,48 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { UsersService } from '../shared/services/users.service';
 import { UserInfoModel } from '../shared/user.model';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.css']
+  styleUrls: ['./my-profile.component.css'],
 })
 export class MyProfileComponent implements OnInit {
   userInfo: UserInfoModel;
-  @ViewChild('f') signupForm:NgForm;
-  data:any;
+  signupForm:FormGroup;
+  data;
+  isDataAvailable = false;
 
-  constructor(private usersService:UsersService){
-  }
+  constructor(private usersService: UsersService) {}
 
-   ngOnInit(){
+  ngOnInit() {
+    // initiate execution
+    
+      // Executer fn...
+      this.fetchData();
+
   
-    this.usersService.getUserData().subscribe( res=>{
-      console.log(res);
-       this.data=res[0];
-    });
-    
-    
   }
 
-  onSubmit(){
-
-    // this.userInfo = new UserInfoModel();
-    // this.userInfo.firstName = this.signupForm.value.firstName;
-    // this.userInfo.lastName = this.signupForm.value.lastName;
-    // this.userInfo.email = this.signupForm.value.email;
-    // this.userInfo.currency = this.signupForm.value.currency;
-    // this.userInfo.income = this.signupForm.value.income;
-    // this.userInfo.password = this.signupForm.value.password;
+  fetchData() {
+    return new Promise((resolve)=>{
+      resolve(this.usersService.getUserData().subscribe((res) => {
+        console.log('ressss', res);
+        this.data = res[0];
+        console.log("data",this.data);
+        
+        this.isDataAvailable=true;
+      }));
+    });
+  }
+  onSubmit() {
+    console.log("form data",this.data);
+    
+    
 
     console.log();
-    
-    
   }
-  
-  submitted(){
 
-}
+  submitted() {}
 }
